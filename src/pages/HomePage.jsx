b436b414f39ec6231f03187f4f8298578c60c588
aian <53809101+deeowemez@users.noreply.github.com>
2024-10-05 22:45:29 +0800

    add components for home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import '../index.css'
 
 import Header from "../components/Header";
-import Footer from "../components/Footer"
+import Headline from "../components/Headline";
+import Features from "../components/Features";
+import Pricing from "../components/Pricing";
+import Footline from "../components/Footline";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
     return (
         <div className="w-screen p-0 m-0 overflow-x-hidden">
             <Header />
+            <Headline />
+            <Features />
+            <Pricing />
+            <Footline />
             <Footer />
         </div>
     )
