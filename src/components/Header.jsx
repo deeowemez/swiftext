4b104e3611e40124e12e2d95711d819b1103d922
aian <53809101+deeowemez@users.noreply.github.com>
2024-10-05 22:29:35 +0800

    add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 const Header = () => {
-    return(
-        <div className="font-sserif">
+    return (
+        <div className="font-sserif text-sm text-[#5A5959] w-screen flex justify-between items-center py-1.5 px-36">
             <div className="flex">
-                <img src="src/assets/images/logo.svg" alt="Swiftext Logo" className="h-8 w-auto" />
-                <p>Swiftext</p>
+                <img src="src/assets/images/logo.svg" alt="Swiftext Logo" className="h-12 w-auto" />
+                <div className="font-title text-3xl gradient-text font-bold py-2 px-1 cursor-pointer">Swiftext</div>
             </div>
+            <nav >
+                <ul className="flex gap-x-10 cursor-pointer">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/edit">Edit</a></li>
+                    <li>Files</li>
+                </ul>
+            </nav>
+            <div className="flex gap-10">
+                <button>
+                    Log In
+                </button>
+                <button className="border-2 border-[#FF903D] rounded-lg px-4 py-2 text-[#FF903D]">
+                    Sign Up
+                </button>
+            </div>
+
         </div>
     )
 }
