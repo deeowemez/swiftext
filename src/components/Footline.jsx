65ce2320fd19cb357549e8d0df4addf21a13b428
Haydee Shane Saguid <137850019+Yahds@users.noreply.github.com>
2024-10-05 23:55:54 +0800

    complete footline layout and design

---
+++ src/components/Footline.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footline = () => {
+    return (
+        <div className="flex items-center justify-center h-[1000px] bg-[#FF903D]" >
+            <div className="font-sserif flex flex-col items-center justify-center grid-rows-[1fr_1fr_1fr]">
+                <ul className="flex flex-col items-center justify-center">
+                    <li className="text-[80px] font-semi-bold pb-1 text-white">Start Annotating Today</li>
+                    <li className="text-[19px] font-light max-w-80 pb-16 text-center text-white ">Format your annotations effortlessly within Swiftext’s streamlined interface</li>
+                    <button className="flex flex-row items-center py-4 px-5 rounded-xl bg-white">
+                        <p className="text-[18px] text-[#FF903D]">Create Free Account</p>
+                        <img src="src/assets/images/arrow-btn.svg" alt="arrow-btn" className='h-10 w-10 cursor-pointer pl-4' />
+                    </button>
+                </ul>
+            </div>
+        </div>
+    )    
+}
+
+export default Footline
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ import '../index.css'
 
 import Header from "../components/Header";
 import Headline from "../components/Headline";
-import Features from "../components/Features";
-import Pricing from "../components/Pricing";
+// import Features from "../components/Features";
+// import Pricing from "../components/Pricing";
 import Footline from "../components/Footline";
 import Footer from "../components/Footer";
 
@@ -13,8 +13,8 @@ const HomePage = () => {
         <div className="w-screen p-0 m-0 overflow-x-hidden">
             <Header />
             <Headline />
-            <Features />
-            <Pricing />
+            {/* <Features /> */}
+            {/* <Pricing /> */}
             <Footline />
             <Footer />
         </div>
