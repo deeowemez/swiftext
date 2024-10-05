4828b6b7ec743d4fffc258b417f888b9388d01da
aian <53809101+deeowemez@users.noreply.github.com>
2024-10-05 22:47:16 +0800

    initialize jsx files for home page components

---
+++ src/components/Headline.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Headline = () => {
+    return(
+        <div className="">
+            <div>
+                <p>Easy-to-use</p>
+                <p>Annotations App</p>
+            </div>
+            <p></p>
+        </div>
+    )
+}
+
+export default Headline;
