c64ee661d0695fa89e439466fda97b2c887ba8e3
aian <53809101+deeowemez@users.noreply.github.com>
2024-10-05 21:24:16 +0800

    sync

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    './src/**/*.{html,js,jsx}',
+    './index.html,'
+  ];
+  
+  export const theme = {
+    extend: {
+      fontFamily: {
+        title: ['Fredoka'],
+        sserif: ['Inter', 'sans-serif'],
+      },
+      backgroundImage: {
+        'teal-gradient': 'linear-gradient(to left, #AAD9D1, #017373)',
+      },
+      textColor: {
+        transparent: 'transparent',
+      },
+      borderColor: {
+        tealOne: '#048C80',
+      },
+    },
+  };
+  
+  export const plugins = [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.gradient-text': {
+          'background-image': 'linear-gradient(to left, #AAD9D1, #017373)',
+          '-webkit-background-clip': 'text',
+          'background-clip': 'text',
+          'color': 'transparent',
+        },
+      });
+    },
+  ];
