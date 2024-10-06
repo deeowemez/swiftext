fb5fb25dd0fc9ea5828b2fb9e87e3ca498d349a3
aian <53809101+deeowemez@users.noreply.github.com>
2024-10-06 12:38:59 +0800

    fix real-time and tagging feature responsiveness

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -40,11 +40,11 @@ const Features = () => {
                     </div>
                 </div>
 
-                {/* Real Time Preview Feature
-                <div className="flex flex-col lg:flex-row items-center justify-start">
-                    <div className="relative">
-                        <img src="src/assets/images/blob/features/ft-blob-3.svg" alt="Features Blob Design" className="m-24 w-[800px]" />
-                        <img src="src/assets/images/gif/real-time-gif.gif" alt="" className="absolute top-72 left-[200px] w-[500px] rounded-[180px]" />
+                {/* Real Time Preview Feature */}
+                <div className="flex flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row items-center justify-start">
+                    <div className="relative w-4/5 lg:w-full xl:w-2/3">
+                        <img src="src/assets/images/blob/features/ft-blob-3.svg" alt="Features Blob Design" className="my-12 md:-20 lg:m-20 xl:m-24 w-full lg:w-5/6 xl:w-5/6 max-w-[800px] " />
+                        <img src="src/assets/images/gif/real-time-gif.gif" alt="" className="absolute top-36 left-12 md:top-48 md:left-[90px] lg:top-64 lg:left-[210px] xl:top-64 xl:left-[196px] w-3/5 lg:w-1/2 xl:w-1/2 max-w-[500px] rounded-[180px]" />
                     </div>
                     <div className="flex flex-col p-7 w-80 h-38 bg-[#FFFCFA] rounded-3xl ">
                         <div className="flex mb-3">
@@ -55,20 +55,20 @@ const Features = () => {
                     </div>
                 </div>
 
-                Tagging Feature
-                <div className="flex flex-col lg:flex-row items-center justify-end">
-                    <div className="flex flex-col p-7 w-80 h-38 bg-[#FFFCFA] rounded-3xl ">
+                {/* Tagging Feature */}
+                <div className="flex flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row items-center justify-end">
+                    <div className="flex flex-col p-7 w-80 h-38 bg-[#FFFCFA] rounded-3xl order-2 xl:order-1  ">
                         <div className="flex mb-3">
                             <img src="src/assets/images/tag.svg" alt="tag image" className="h-6 mr-3" />
                             <p className="font-sserif text-xl text-black font-medium">Tagging & Organization</p>
                         </div>
                         <p className="font-dmsans text-sm text-[#737373]">Keep your files organized with custom tags and categories. Use filters to quickly find specific documents. </p>
                     </div>
-                    <div className="relative">
-                        <img src="src/assets/images/blob/features/ft-blob-4.svg" alt="Features Blob Design" className="m-24 w-[800px]" />
-                        <img src="src/assets/images/gif/tag-gif.gif" alt="" className="absolute top-72 right-[220px] w-[580px] rounded-[180px]" />
+                    <div className="relative w-4/5 lg:w-full xl:w-2/3 order-1 xl:order-2">
+                        <img src="src/assets/images/blob/features/ft-blob-4.svg" alt="Features Blob Design" className="my-12 md:-20 lg:m-20 xl:m-24 w-full lg:w-5/6 xl:w-5/6 max-w-[800px]" />
+                        <img src="src/assets/images/gif/tag.gif" alt="" className="absolute top-[130px] left-[70px] md:top-[160px] md:left-[110px] lg:top-60 lg:left-[200px] xl:top-[240px] xl:left-[220px] w-3/5 lg:w-1/2 xl:w-6/12 max-w-[600px] rounded-[80px] xl:rounded-[90px] " />
                     </div>
-                </div>  */}
+                </div>  
 
             </div>
         </div>
