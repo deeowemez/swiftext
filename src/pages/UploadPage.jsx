a0ff506fbe6aceb598ecf6740598d9f4dbac82b0
Haydee Shane Saguid <137850019+Yahds@users.noreply.github.com>
2024-10-07 19:04:41 +0800

    update uploading effect

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -6,14 +6,13 @@ import Footer from "../components/Homepage/Footer";
 
 const UploadPage = () => {
     const fileInputRef = useRef(null);
-    const [isGifVisible, setIsGifVisible] = useState(false);
+    const [isVideoVisible, setIsVideoVisible] = useState(false);
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         if (file) {
             console.log("File uploaded:", file);
-            // Show the video overlay
-            setIsGifVisible(true);
+            setIsVideoVisible(true);
         }
     };
 
@@ -22,8 +21,7 @@ const UploadPage = () => {
         const file = event.dataTransfer.files[0];
         if (file) {
             console.log("File dropped:", file);
-            // Show the video overlay
-            setIsGifVisible(true);
+            setIsVideoVisible(true);
         }
     };
 
@@ -35,48 +33,25 @@ const UploadPage = () => {
         fileInputRef.current.click();
     };
 
-    const handleGifEnd = () => {
-        // Hide the video overlay after it finishes
-        setTimeout(() => {
-            setIsGifVisible(false);
-        }, 3000);
+    const handleVideoEnd = () => {
+        setIsVideoVisible(false);
     };
 
     return (
         <div className="flex flex-col w-screen p-0 m-0 overflow-x-hidden relative min-h-screen">
             <Header />
-            
-            {isGifVisible && (
+            {isVideoVisible && (
                 <div className="fixed top-0 left-0 flex flex-col w-full h-full justify-center items-center bg-white z-50 opacity-90">
-                    <img 
-                        src="src/assets/images/gif/upload-gif.gif" 
-                        alt="uploadin effect" 
-                        className="w-[1100px] h-[700px] p-10 z-10" 
-                        onLoad={handleGifEnd}
-                    
+                    <video src="upload-video.mp4" autoPlay muted className="w-[1100px] h-[700px] p-10 z-10"onEnded={handleVideoEnd}
                     />
                 </div>
             )}
 
-            <div 
-                className="flex flex-grow justify-center items-center border-[1px]"
-                onDrop={handleDrop} 
-                onDragOver={handleDragOver}
-            >
+            <div className="flex flex-grow justify-center items-center border-[1px]"onDrop={handleDrop} onDragOver={handleDragOver}>
                 <div className="flex flex-col justify-center items-center rounded-lg border-dashed border-2 border-gray-300 hover:border-gray-400 cursor-pointer w-3/4 h-2/3 m-32 p-32" onClick={handleImageClick}>
-                    <img 
-                        src="src/assets/images/upload-2.svg" 
-                        alt="Upload symbol" 
-                        className="flex" 
-                    />
+                    <img src="src/assets/images/upload-2.svg" alt="Upload symbol" className="flex" />
                     <div className="font-sserif text-[#A3A3A3] font-semibold">Upload PDF/Word File</div>
-                    <input 
-                        type="file" 
-                        accept=".pdf,.doc,.docx" 
-                        ref={fileInputRef} 
-                        onChange={handleFileChange} 
-                        className="hidden" 
-                    />
+                    <input type="file" accept=".pdf,.doc,.docx" ref={fileInputRef} onChange={handleFileChange} className="hidden" />
                 </div>
             </div>
 
