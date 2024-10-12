eab3afeee35ab2c456a9b60f457c28c776ad5f48
aian <53809101+deeowemez@users.noreply.github.com>
2024-10-12 22:21:36 +0800

    add HTTP request to retrieve files from backend

--- src/pages/FilesPage.jsx
+++ src/pages/FilesPage.jsx
@@ -1,13 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axios from 'axios';
 import Header from "../components/Homepage/Header";
 import Footer from "../components/Homepage/Footer";
+
 
 const FilesPage = () => {
     const fileInputRef = useRef(null);
     const [selectedFile, setSelectedFile] = useState(null);
     const [uploadStatus, setUploadStatus] = useState('');
-    const [uploadedFiles, setUploadedFiles] = useState([]); // State to store uploaded files
+    const [files, setFiles] = useState([]); // State to store uploaded files    
+
+    useEffect(() => {
+        const fetchFiles = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/uploads');
+                console.log('response data: ', response.data);
+                setFiles(response.data);
+            } catch (error) {
+                console.error('Error fetching files:', error);
+            }
+        };
+
+        fetchFiles();
+    });
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -18,7 +33,7 @@ const FilesPage = () => {
     };
 
     const handleFileUpload = async (file) => {
-        if (!file || file == null) {
+        if (!file) {
             setUploadStatus('Please select a file to upload.');
             return;
         }
@@ -34,7 +49,7 @@ const FilesPage = () => {
             });
 
             // Update uploaded files array with the new file
-            setUploadedFiles((prevFiles) => [...prevFiles, { name: file.name, lastModified: file.lastModified }]);
+            setFiles((prevFiles) => [...prevFiles, { name: file.name, lastModified: file.lastModified }]);
             setUploadStatus(`Upload successful: ${response.data.fileName}`);
         } catch (error) {
             setUploadStatus('Upload failed. Please try again.');
@@ -86,13 +101,13 @@ const FilesPage = () => {
                 </div>
                 <div className="file-cards flex flex-col gap-2 py-6 w-full">
                     {/* Dynamically create file cards */}
-                    {uploadedFiles.map((file, index) => (
+                    {files.map((file, index) => (
                         <div key={index} className="flex items-center justify-start bg-[#F4F4F4] rounded-md h-12 w-full min-w-full max-w-full gap-3 px-5 py-9 cursor-pointer">
                             <div className="bg-[#333333] w-1/12 text-white text-center ">thumbnail prev</div>
                             <div className="flex flex-col w-11/12">
-                                <p className="text-left text-md overflow-clip">{file.name}</p>
+                                <p className="text-left text-md overflow-clip">{file.filename}</p>
                                 <div className="flex justify-between">
-                                    <p className="text-right text-xs italic">{new Date(file.lastModified).toLocaleDateString()}</p>
+                                    <p className="text-right text-xs italic">{new Date(file.last_modified).toLocaleTimeString()}</p>
                                     <div className="flex gap-4 items-center">
                                         <div className="bg-[#F8968E] w-3 h-3 rounded-full"></div>
                                         <img src="src/assets/images/more-alt.svg" alt="more svg" />
