e014e8de2592523d789e4cb7d9a121ecd8f83ea8
aian <53809101+deeowemez@users.noreply.github.com>
2024-12-25 07:52:10 +0800

    modify dir structure

--- src/components/EditPage/types.ts
+++
@@ -1,9 +0,0 @@
-import { Highlight, Content } from "react-pdf-highlighter-extended";
-
-export interface CommentedHighlight extends Highlight {
-  content: Content;
-  comment?: string;
-  color: string;
-  configID: string;
-  profileID: string;
-}
