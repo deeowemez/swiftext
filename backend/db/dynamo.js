3ee2fafb991b0a2050bfe7932fc8b6262696bb86
aian <53809101+deeowemez@users.noreply.github.com>
2025-01-30 18:22:13 +0800

    update env var

--- backend/db/dynamo.js
+++ backend/db/dynamo.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const dynamoDB = new DynamoDBClient({
   region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT, 
+  endpoint: process.env.AWS_DYNAMO_ENDPOINT,
   // credentials: {
   //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
