c89027b9f41c6beaf8a7f99a6e39f2e97a0cc438
aian <53809101+deeowemez@users.noreply.github.com>
2025-02-12 16:23:36 +0800

    modify react router config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect } from "react";
 
 import HomePage from './pages/HomePage';
