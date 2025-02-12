81da5f459a2a860b7b1ef3131269a5f810a12c8a
aian <53809101+deeowemez@users.noreply.github.com>
2025-02-12 16:42:22 +0800

    modify statif json file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect } from "react";
 
 import HomePage from './pages/HomePage';
